5c46188c4367f279d124942ff2d2029d19fe757b
MeddyBalont <Meddy.Balont@hotmail.com>
2024-02-06 17:58:17 +0100

    useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
+import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 import QuestionForm from './QuestionForm'
 
 
 
+
 function App() {
 	return (
 		<div>
+			
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			{/* <Cart /> */}
-			<QuestionForm />
+			<div className='lmj-layout-inner'></div>
+			<Cart /> 
 			<ShoppingList />
+			<QuestionForm />
 			<Footer />
 		</div>
 	)
--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,9 +1,11 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+// Ici, il s'agit d'une manière de faire.
+//Vous auriez aussi pu utiliser une fonction qui retourne l'élément souhaité, ou bien faire directement des conditions
 const quantityLabel = {
-	1: 'un peu',
-	2: 'modérament',
+	1: 'peu',
+	2: 'modérément',
 	3: 'beaucoup'
 }
 
@@ -18,7 +20,7 @@ function CareScale({ scaleValue, careType }) {
 
 	return (
 		<div
-			onClick={()=>
+			onClick={() =>
 				alert(
 					`Cette plante requiert ${quantityLabel[scaleValue]} ${
 						careType === 'light' ? 'de lumière' : "d'arrosage"
