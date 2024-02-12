ef443790f9b9cc3b9aa9a5647e437dd8dd1ba177
MeddyBalont <Meddy.Balont@hotmail.com>
2024-02-12 15:57:55 +0100

    Last commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,19 @@ import logo from '../assets/logo.png'
 import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
-import QuestionForm from './QuestionForm'
-
-
-
+import '../styles/Layout.css'
 
 function App() {
 	return (
 		<div>
-			
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			<div className='lmj-layout-inner'></div>
-			<Cart /> 
-			<ShoppingList />
-			<QuestionForm />
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
 			<Footer />
 		</div>
 	)
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,36 +1,34 @@
-import { plantList } from '../datas/PlantList';
-import PlantItem from './PlantItem';
-import '../styles/ShoppingList.css';
+import { plantList } from '../datas/PlantList'
+import PlantItem from './PlantItem'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-  const categories = plantList.reduce(
-    (acc, plant) =>
-      acc.includes(plant.category) ? acc : acc.concat(plant.category),
-    []
-  );
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-  return (
-    <div>
-      <ul>
-        {categories.map((cat) => (
-          <li key={cat}>{cat}</li>
-        ))}
-      </ul>
-      <ul className="lmj-plant-list">
-        {plantList.map(({ id, cover, name, water, light }) => (
-          <PlantItem
-            // Ajout de cette ligne afin de garantir que chaque élément généré a une clé unique (permet de gérer les mises à jour efficacement et éviter les doublons, la key indique à React que c'est l'attribut unique qui permet de différencier les items entre eux, ici ta clé unique = l'id, mais react peut pas le deviner)
-            key={id}
-            id={id}
-            cover={cover}
-            name={name}
-            water={water}
-            light={light}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='lmj-shopping-list'>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						key={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default ShoppingList;
+export default ShoppingList
